5be5bfe2499254096a9d349d6b5870e16f67cd77
mostafaGwely <mtgwely0100@gmail.com>
2019-02-21 18:09:52 +0200

    the cleaned project

--- keras_test.py
+++ keras_test.py
@@ -1,19 +1,32 @@
-
-# coding: utf-8
-
-# In[3]:
-
-
+# set the matplotlib backend so figures can be saved in the background
+# import matplotlib
+# matplotlib.use("Agg")
+ 
+# import the necessary packages
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.optimizers import SGD
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
 import argparse
+import random
+import pickle
+import cv2
+import os
+
+# ap = argparse.ArgumentParser()
+
+# ap.add_argument('-d','--dataset',required=True,help="path to input dataset of images")
+# ap.add_argument('-m','--model',required=True,help="path to the output trained model")
+# ap.add_argument('-l','--label-bin',required=True,help="path to output label binarize")
+# ap.add_argument('-p','--plot',required=True,help='path to the output accuracy/loss plot')
+
+# args = vars(ap.parse_args())
 
 
-# In[4]:
 
-
-ap = argparse.ArgumentParser()
-ap.add_argument('-d','--dataset',required=True,help="path to input dataset of images")
-ap.add_argument('-m','--model',required=True,help="path to the output trained model")
-ap.add_argument('-l','--label-bin',required=True,help="path to output label binarize")
-ap.add_argument('-p','--plot',required=True,help='path to the output accuracy/loss plot')
-args = vars(ap.parse_args())
 
